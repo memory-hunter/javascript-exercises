ef077868ed540f15207c6b72a2fdefdc23b7fc44
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2024-05-03 23:38:07 +0400

    Add solutions exercise 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    let sizeOfElements = arguments.length;
+    for (let i = 1; i < sizeOfElements; i++) {
+        for (let j = 0; j < array.length;) {
+            if (array[j] === arguments[i]) {
+                array.splice(j, 1);
+            }
+            else {
+                j++;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result = 0;
+    if (typeof start !== "number" || typeof end !== "number" || start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
