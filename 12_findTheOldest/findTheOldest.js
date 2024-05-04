9c9c8619ad319de44a633a323270ee0405851c25
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2024-05-05 00:55:37 +0400

    Add last solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  array = array.sort(
+    (x, y) =>
+      (y.yearOfDeath === undefined ? 2024 : y.yearOfDeath) -
+      y.yearOfBirth -
+      ((x.yearOfDeath === undefined ? 2024 : x.yearOfDeath) - x.yearOfBirth)
+  );
+  console.log(array);
+  return array[0];
 };
 
 // Do not edit below this line
