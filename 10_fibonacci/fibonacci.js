f7680e24399553d9b557008536cfba5fc6674f69
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2024-05-05 00:38:56 +0400

    Add solution to exercise tenth

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberUpto) {
+    if (numberUpto <= 0) {
+        return numberUpto == 0 ? 0 : "OOPS";
+    }
+    let arr = [1, 1];
+    for(let i = 1; i < parseInt(numberUpto); i++) {
+        arr[i+1] = arr[i] + arr[i-1];
+    }
+    return arr[numberUpto - 1];
 };
 
 // Do not edit below this line
