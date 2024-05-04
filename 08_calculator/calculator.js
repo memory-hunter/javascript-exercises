62c7fd188aa471524dd91a36b77e96bed0453f6f
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2024-05-05 00:05:19 +0400

    Add solution for the eight exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return parseInt(a) + parseInt(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return parseInt(a) - parseInt(b);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, element) => {
+    return total + element;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, element) => {
+    return total * element;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return parseInt(a) ** parseInt(b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  return number == 0 ? 1 : function () {
+    let result = 1;
+    for (let i = number; i > 0; i--) {
+      result *= i;
+    }
+    return result;
+  }();
 };
 
 // Do not edit below this line
