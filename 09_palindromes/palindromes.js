db79ae6935cd1e0fc8066a24ee6b2a4250a8640f
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2024-05-05 00:23:31 +0400

    Add solution to exercsie nineth

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    strarr = Array.from(string.toLowerCase());
+    processed = strarr.filter(c => c.match(/^[a-z0-9]+$/i));
+    let flipped = processed.slice().reverse();
+    return processed.toString() == flipped.toString();
 };
 
 // Do not edit below this line
